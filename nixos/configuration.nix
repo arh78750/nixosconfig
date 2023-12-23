ae429e2648a57fcd8e08b6ecf3d8e7822963ab68
andrew <andrew@andrewhall.site>
2023-12-22 22:41:41 -0800

    initial commit!!

---
+++ nixos/configuration.nix
@@ -0,0 +1,221 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [   # Include the results of the hardware scan.
+        ./hardware-configuration.nix
+        # Include Home Manager
+        # ./home.nix
+    ];
+
+  # Bootloader
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Networking
+  networking.networkmanager.enable = true;
+  networking.hostName = "phantom"; # Define your hostname.
+
+  # Set your time zone.
+  time.timeZone = "America/Los_Angeles";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.andrew = {
+    isNormalUser = true;
+    description = "andrew";
+    extraGroups = [ "networkmanager" "wheel" ];
+  };
+
+  programs.hyprland.enable = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    # Utils
+    vim
+    wget
+    btop
+    git
+    unzip
+    swaynotificationcenter
+    # Misc
+    wl-clipboard
+    cowsay
+    neofetch
+    pkg-config
+    lolcat
+    # Photo & Video
+    mpv
+    obs-studio
+    # Online
+    firefox
+    discord
+    # Dev
+    glibc
+    # Audio
+    pavucontrol
+    goxlr-utility
+    # Fonts
+    font-awesome
+    symbola
+    noto-fonts-color-emoji
+    material-icons
+  ];
+
+  fonts.packages = with pkgs; [
+    (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+  ];
+
+  # Steam Configuration
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+  };
+
+  # OpenGL
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  # Nix Package Management
+  nix = {
+    settings.auto-optimise-store = true;
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
+  };
+
+  # Set Environment Variables
+  environment.variables={
+   NIXOS_OZONE_WL = "1";
+   PATH = [
+     "\${HOME}/.local/bin"
+     "\${HOME}/.cargo/bin"
+     "\$/usr/local/bin"
+   ];
+   NIXPKGS_ALLOW_UNFREE = "1";
+   XDG_CURRENT_DESKTOP = "Hyprland";
+   XDG_SESSION_TYPE = "wayland";
+   XDG_SESSION_DESKTOP = "Hyprland";
+   GDK_BACKEND = "wayland";
+   CLUTTER_BACKEND = "wayland";
+   SDL_VIDEODRIVER = "x11";
+   QT_QPA_PLATFORM = "wayland";
+   QT_QPA_PLATFORMTHEME = "qt5ct";
+   QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+   QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+   MOZ_ENABLE_WAYLAND = "1";
+   # NVIDIA stuff
+   LIBVA_DRIVER_NAME = "nvidia";
+   GBM_BACKEND = "nvidia-drm";
+   __GLX_VENDOR_LIBRARY_NAME = "nivida";
+   WLR_NO_HARDWARE_CURSORS = "1";
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    powerManagement.enable = false;
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of 
+    # supported GPUs is at: 
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+    # Only available from driver 515.43.04+
+    # Currently alpha-quality/buggy, so false is currently the recommended setting.
+    open = false;
+
+    # Enable the Nvidia settings menu,
+	# accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+
+  # List services that you want to enable:
+  services.openssh.enable = true;
+  services.fstrim.enable = true;
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+    libinput.enable = true;
+  };
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+  hardware.pulseaudio.enable = false;
+  sound.enable = true;
+  security.rtkit.enable = true;
+
+  # Automatic Updates
+  system.autoUpgrade = {
+    enable = true;
+    channel = "https://nixos.org/channels/nixos-23.11";
+  };
+  
+  nixpkgs.config.allowUnfree = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+
+}

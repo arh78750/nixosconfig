2cb6eb9ac06948a7c15f536ad2f812af11efe82a
andrew <andrew@andrewhall.site>
2023-12-23 20:47:13 -0800

    working pog

--- flake.nix
+++ flake.nix
@@ -3,18 +3,19 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-23.11";
+
     home-manager = {
       url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    neovim-flake = {
+      url = "github:jordanisaacs/neovim-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    }
   };
 
-  outputs = inputs@{
-    self,
-    nixpkgs,
-    home-manager,
-    ...
-  }: let
+  outputs = inputs@{ self, nixpkgs, home-manager, neovim-flake, ... }:
+  let
     # -- SYSTEM SETTINGS -- #
     system = "x86_64-linux";
     hostname = "phantom";
@@ -27,11 +28,11 @@
         inherit system;
         modules = [
           ./nixos/configuration.nix
-          home-manager.nixosModules.home-manager.home-manager
-          {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            users.andrew = import ./nixos/home.nix;
+
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.andrew = import ./nixos/home.nix;
           }
         ];
         specialArgs = {
@@ -40,7 +41,6 @@
           inherit username;
           inherit name;
         };
-
       };
     };
   };
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -11,6 +11,7 @@
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
     ../modules/hardware/goxlr.nix
+    ../modules/programs/neovim.nix
   ];
 
   # Bootloader

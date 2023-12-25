1749891911a13b53d96a8b6879d882c7778b4fc1
andrew <andrew@andrewhall.site>
2023-12-24 20:12:14 -0800

    small reformat to make common configuration.nix

---
+++ profiles/common/configuration.nix
@@ -0,0 +1,208 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+{
+  config,
+  pkgs,
+  unstable,
+  username,
+  system,
+  neovim-flake,
+  inputs,
+  ...
+}: {
+  imports = [
+    # Include the results of the hardware scan.
+    ../../hardware-configuration.nix
+    ../../modules/hardware/goxlr.nix
+    ../../modules/programs/neovim.nix
+    ../../modules/programs/clonehero.nix
+  ];
+
+  # Bootloader
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Networking
+  networking.networkmanager.enable = true;
+  networking.hostName = "phantom"; # Define your hostname.
+
+  # Set your time zone.
+  time.timeZone = "America/Los_Angeles";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.${username} = {
+    isNormalUser = true;
+    description = username;
+    extraGroups = ["networkmanager" "wheel"];
+  };
+
+  users.defaultUserShell = pkgs.zsh;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    # Utils
+    vim
+    wget
+    lsd
+    btop
+    git
+    unzip
+    swaynotificationcenter
+    wofi
+    swww
+    # Misc
+    wl-clipboard
+    cowsay
+    neofetch
+    pkg-config
+    lolcat
+    tldr
+    # Photo & Video
+    mpv
+    obs-studio
+    # Online
+    firefox
+    discord-canary
+    xwaylandvideobridge
+    #    xdg-desktop-portal-hyprland
+    # Dev
+    glibc
+    # Audio
+    pavucontrol
+    goxlr-utility
+    # Fonts
+    font-awesome
+    symbola
+    noto-fonts-color-emoji
+    material-icons
+  ];
+
+  fonts.packages = with pkgs; [
+    (nerdfonts.override {fonts = ["JetBrainsMono"];})
+  ];
+
+  # Steam Configuration
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+  };
+
+  # OpenGL
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  # Nix Package Management
+  nix = {
+    settings.auto-optimise-store = true;
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
+  };
+
+  # Set Environment Variables
+  environment.variables = {
+    NIXOS_OZONE_WL = "1";
+    PATH = [
+      "\${HOME}/.local/bin"
+      "\${HOME}/.cargo/bin"
+      "\$/usr/local/bin"
+    ];
+    NIXPKGS_ALLOW_UNFREE = "1";
+    XDG_CURRENT_DESKTOP = "Hyprland";
+    XDG_SESSION_TYPE = "wayland";
+    XDG_SESSION_DESKTOP = "Hyprland";
+    GDK_BACKEND = "wayland";
+    CLUTTER_BACKEND = "wayland";
+    SDL_VIDEODRIVER = "x11";
+    QT_QPA_PLATFORM = "wayland";
+    QT_QPA_PLATFORMTHEME = "qt5ct";
+    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+    MOZ_ENABLE_WAYLAND = "1";
+    # NVIDIA stuff
+    #    LIBVA_DRIVER_NAME = "nvidia";
+    #    GBM_BACKEND = "nvidia-drm";
+    #    __GLX_VENDOR_LIBRARY_NAME = "nivida";
+    #    WLR_NO_HARDWARE_CURSORS = "1";
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  # List services that you want to enable:
+  services.openssh.enable = true;
+  services.fstrim.enable = true;
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+    libinput.enable = true;
+  };
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+  hardware.pulseaudio.enable = false;
+  sound.enable = true;
+  security.rtkit.enable = true;
+
+  # Automatic Updates
+  system.autoUpgrade = {
+    enable = true;
+    channel = "https://nixos.org/channels/nixos-23.11";
+  };
+
+  nixpkgs.config = {
+    # Allow proprietary packages
+    allowUnfree = true;
+  };
+  nix.settings = {
+    experimental-features = ["nix-command" "flakes"];
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+}
--- profiles/development/configuration.nix
+++ profiles/development/configuration.nix
@@ -13,46 +13,8 @@
 }: {
   imports = [
     # Include the results of the hardware scan.
-    ./hardware-configuration.nix
-    ../../modules/hardware/goxlr.nix
-    ../../modules/programs/neovim.nix
-    ../../modules/programs/clonehero.nix
+    ../common/configuration.nix
   ];
-
-  # Bootloader
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  # Networking
-  networking.networkmanager.enable = true;
-  networking.hostName = "phantom"; # Define your hostname.
-
-  # Set your time zone.
-  time.timeZone = "America/Los_Angeles";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.${username} = {
-    isNormalUser = true;
-    description = username;
-    extraGroups = ["networkmanager" "wheel"];
-  };
-
-  users.defaultUserShell = pkgs.zsh;
 
   programs.hyprland.enable = true;
   programs.zsh.enable = true;
@@ -66,62 +28,12 @@
   environment.systemPackages = with pkgs; [
     # Utils
     (import ../../modules/scripts/autohypr.nix {inherit pkgs;})
-    vim
-    wget
-    lsd
-    btop
-    git
-    unzip
-    swaynotificationcenter
-    wofi
-    swww
-    # Misc
-    wl-clipboard
-    cowsay
-    neofetch
-    pkg-config
-    lolcat
-    # Photo & Video
-    mpv
-    obs-studio
-    # Online
-    firefox
-    discord-canary
-    xwaylandvideobridge
-    #    xdg-desktop-portal-hyprland
-    # Dev
-    glibc
-    # Audio
-    pavucontrol
-    goxlr-utility
-    # Fonts
-    font-awesome
-    symbola
-    noto-fonts-color-emoji
-    material-icons
   ];
 
-  fonts.packages = with pkgs; [
-    (nerdfonts.override {fonts = ["JetBrainsMono"];})
-  ];
   # goxlr configuration
   services.goxlr-utility = {
     autoStart.xdg = true;
     enable = true;
-  };
-
-  # Steam Configuration
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true;
-    dedicatedServer.openFirewall = true;
-  };
-
-  # OpenGL
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
   };
 
   # Nix Package Management
@@ -136,37 +48,11 @@
 
   # Set Environment Variables
   environment.variables = {
-    NIXOS_OZONE_WL = "1";
-    PATH = [
-      "\${HOME}/.local/bin"
-      "\${HOME}/.cargo/bin"
-      "\$/usr/local/bin"
-    ];
-    NIXPKGS_ALLOW_UNFREE = "1";
-    XDG_CURRENT_DESKTOP = "Hyprland";
-    XDG_SESSION_TYPE = "wayland";
-    XDG_SESSION_DESKTOP = "Hyprland";
-    GDK_BACKEND = "wayland";
-    CLUTTER_BACKEND = "wayland";
-    SDL_VIDEODRIVER = "x11";
-    QT_QPA_PLATFORM = "wayland";
-    QT_QPA_PLATFORMTHEME = "qt5ct";
-    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
-    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
-    MOZ_ENABLE_WAYLAND = "1";
     # NVIDIA stuff
     LIBVA_DRIVER_NAME = "nvidia";
     GBM_BACKEND = "nvidia-drm";
     __GLX_VENDOR_LIBRARY_NAME = "nivida";
     WLR_NO_HARDWARE_CURSORS = "1";
-  };
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  programs.mtr.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
   };
 
   # Load nvidia driver for Xorg and Wayland
@@ -198,53 +84,4 @@
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
-
-  # List services that you want to enable:
-  services.openssh.enable = true;
-  services.fstrim.enable = true;
-  services.xserver = {
-    layout = "us";
-    xkbVariant = "";
-    libinput.enable = true;
-  };
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
-  };
-  hardware.pulseaudio.enable = false;
-  sound.enable = true;
-  security.rtkit.enable = true;
-
-  # Automatic Updates
-  system.autoUpgrade = {
-    enable = true;
-    channel = "https://nixos.org/channels/nixos-23.11";
-  };
-
-  nixpkgs.config = {
-    # Allow proprietary packages
-    allowUnfree = true;
-  };
-  nix.settings = {
-    experimental-features = ["nix-command" "flakes"];
-    substituters = ["https://hyprland.cachix.org"];
-    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
-  };
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
 }
